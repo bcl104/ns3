3d6a1f3544b2c5c765942947b17299f2e977b241
bcl104 <will@server>
2014-04-17 20:42:59 +0800

    by will

---
+++ scratch/channel-scan-handler-test.cc
@@ -0,0 +1,34 @@
+/*
+ * channel-scan-handler-test.cc
+ *
+ *  Created on: 2014年4月17日
+ *      Author: will
+ */
+#include "ns3/core-module.h"
+#include "ns3/lifi-module.h"
+
+using namespace ns3;
+
+int main ()
+{
+	LogComponentEnable("LifiChannelScanHandler", LOG_LEVEL_FUNCTION);
+	LifiChannelScanHandler handler;
+	LifiMacPibAttribute attribtes;
+	handler.SetMacPibAttribtes(&attribtes);
+	handler.Start(INACTIVE_SCAN, 5000);
+	LifiMacBeacon beacon;
+	Ptr<Packet> p = beacon.GetPacket();
+	LifiMacHeader header;
+	header.SetFrameType(BEACON);
+	header.SetSrcAddress(Address (Mac16Address ("11:12")));
+	header.SetDstAddress(Address (Mac64Address ("21:22:23:24:25:26:27:28")));
+	p->AddHeader(header);
+	Simulator::Schedule(Seconds(30), &LifiChannelScanHandler::ReceiveBeacon, &handler, 30, p);
+	Simulator::Run();
+	Simulator::Stop(Seconds(100));
+
+	return 0;
+}
+
+
+
