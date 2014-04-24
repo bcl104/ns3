6a432cbf5330f3f8917f5861f9862dc8caa20963
bcl104 <bcl1104@163.com>
2014-04-24 13:06:50 +0800

    13:04 st125475466:total 3 files

---
+++ src/lifi/model/lifi-spectrum-loss-model.cc
@@ -0,0 +1,66 @@
+
+/*
+ * lifi-spectrum-loss-model.cc
+ *
+ *  Created on: 2014年4月24日
+ *      Author: st125475466
+ */
+
+#include "lifi-spectrum-loss-model.h"
+#include <ns3/log.h>
+#include <ns3/lifi-indoor-propagation-loss-model.h>
+
+NS_LOG_COMPONENT_DEFINE ("SpectrumPropagationLossModel");
+
+namespace ns3 {
+LifiSpectrumPropagationLossModel::LifiSpectrumPropagationLossModel() {
+	m_antennaParameters->DetectorArea = 0.01;
+	m_antennaParameters->FilterGain = 1.0;
+	m_antennaParameters->ConcentratorGain = 1.0;
+	m_antennaParameters->HalfPowerDegree = (double)15/180*M_PI;
+	m_antennaParameters->FOV = (double)120/180*M_PI;
+	m_antennaParameters->TransmitterDegree = (double)30/180*M_PI;
+	m_antennaParameters->ReceiverDegree = (double)30/180*M_PI;
+}
+
+ns3::LifiSpectrumPropagationLossModel::~LifiSpectrumPropagationLossModel() {
+}
+
+Ptr<SpectrumValue> LifiSpectrumPropagationLossModel::DoCalcRxPowerSpectralDensity(
+	Ptr<const SpectrumValue> txPsd, Ptr<const MobilityModel> a,
+	Ptr<const MobilityModel> b) {
+	Ptr<SpectrumValue> rxPsd = txPsd->Copy();
+	Values::iterator beg = rxPsd->ValuesBegin();
+	Values::iterator end = rxPsd->ValuesEnd();
+	while(beg != end){
+		*beg = DoCalcRxPower(*beg,a,b);
+		beg++;
+	}
+	return rxPsd;
+}
+
+double LifiSpectrumPropagationLossModel::DoCalcRxPower(double txPowerDbm, Ptr<MobilityModel> a, Ptr<MobilityModel> b){
+	double distance = a->GetDistanceFrom (b);
+	double m_temp1 = log(2);
+	double m_temp2 = log(cosl(m_antennaParameters->HalfPowerDegree));
+	double m = -m_temp1/m_temp2;
+	double R0 = ((m+1)/(2*M_PI))*pow(cosl(m_antennaParameters->TransmitterDegree),m);
+			if(m_antennaParameters->ReceiverDegree > m_antennaParameters->FOV)
+			{
+				double A_D_temp = m_antennaParameters->DetectorArea/pow(distance,2);
+				double H = A_D_temp*R0*m_antennaParameters->FilterGain*m_antennaParameters->ConcentratorGain*cosl(m_antennaParameters->ReceiverDegree);
+				double rx = 10*log(H*1000);
+				return rx+txPowerDbm;
+			}
+			else
+				return 0;
+}
+
+void LifiSpectrumPropagationLossModel::SetLifiAntennaParameters(Ptr<LifiAntennaParameters> parameters){
+	m_antennaParameters = parameters;
+}
+
+Ptr<LifiAntennaParameters> LifiSpectrumPropagationLossModel::GetLifiAntennaParameters(){
+	return m_antennaParameters;
+}
+}
---
+++ src/lifi/model/lifi-spectrum-loss-model.h
@@ -0,0 +1,88 @@
+/*
+ * lifi-spectrum-loss-model.h
+ *
+ *  Created on: 2014年4月24日
+ *      Author: st125475466
+ */
+
+#ifndef LIFI_SPECTRUM_LOSS_MODEL_H_
+#define LIFI_SPECTRUM_LOSS_MODEL_H_
+#include <ns3/object.h>
+#include <ns3/mobility-model.h>
+#include <ns3/spectrum-value.h>
+
+namespace ns3 {
+struct LifiAntennaParameters;
+/**
+ * \ingroup spectrum
+ *
+ * \brief spectrum-aware propagation loss model
+ *
+ * Interface for propagation loss models to be adopted when
+ * transmissions are modeled with a power spectral density by means of
+ * the SpectrumValue class.
+ *
+ */
+class LifiSpectrumPropagationLossModel: public SpectrumPropagationLossModel {
+public:
+	LifiSpectrumPropagationLossModel();
+	virtual ~LifiSpectrumPropagationLossModel();
+	void SetLifiAntennaParameters(Ptr<LifiAntennaParameters> parameters);
+	Ptr<LifiAntennaParameters> GetLifiAntennaParameters();
+
+//  static TypeId GetTypeId ();
+//
+//
+//  /**
+//   * used to chain various instances of SpectrumPropagationLossModel
+//   *
+//   * @param next
+//   */
+//  void SetNext (Ptr<SpectrumPropagationLossModel> next);
+//
+//  /**
+//   * This method is to be called to calculate
+//   *
+//   * @param txPsd the SpectrumValue representing the power spectral
+//   * density of the transmission. Watt units are to be used for radio
+//   * communications, and Pascal units for acoustic communications
+//   * (e.g. underwater).
+//   *
+//   * @param a sender mobility
+//   * @param b receiver mobility
+//   *
+//   * @return set of values vs frequency representing the received
+//   * power in the same units used for the txPower parameter.
+//   */
+//  Ptr<SpectrumValue> CalcRxPowerSpectralDensity (Ptr<const SpectrumValue> txPsd,
+//                                                 Ptr<const MobilityModel> a,
+//                                                 Ptr<const MobilityModel> b) const;
+//
+//protected:
+//  virtual void DoDispose ();
+
+private:
+	/**
+	 *
+	 *
+	 * @param txPsd set of values vs frequency representing the
+	 * transmission power. See SpectrumChannel for details.
+	 * @param a sender mobility
+	 * @param b receiver mobility
+	 *
+	 * @return set of values vs frequency representing the received
+	 * power in the same units used for the txPower parameter.
+	 */
+	virtual Ptr<SpectrumValue> DoCalcRxPowerSpectralDensity(
+			Ptr<const SpectrumValue> txPsd, Ptr<const MobilityModel> a,
+			Ptr<const MobilityModel> b);
+
+//  Ptr<SpectrumPropagationLossModel> m_next;
+private:
+	double DoCalcRxPower(double txPowerDbm, Ptr<MobilityModel> a, Ptr<MobilityModel> b);
+	Ptr<LifiAntennaParameters> m_antennaParameters;
+};
+
+} // namespace ns3
+
+#endif /* LIFI_SPECTRUM_LOSS_MODEL_H_ */
